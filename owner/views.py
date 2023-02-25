62d34e157340d4f178f72bc4d25a57be08d686f0
Alexander D. Astuhuaman <alhexanderad@gmail.com>
2023-02-25 13:50:38 -0500

    visualizando data_context en plantilla

--- owner/views.py
+++ owner/views.py
@@ -1,16 +1,70 @@
 from django.shortcuts import render
-from .models import  Owner
 
 
 def owner(request):
-  text = "App Owner funcionando correctamente!!!"
+  text = 'App Owner funcionando correctamente!!!'
   context = {
     'text': text,
-    'title': "Owner",
+    'title': 'Owner',
   }
   return render(request, './owner/owner.html', context)
 
-
 def list_owner(request):
-  owners = Owner.objects.all()
-  return render(request, './owner/owner_list.html', {'owners': owners})
+  data_context = [
+      {
+        'nombre': 'Luis I',
+        'edad': 25,
+        'nacionalidad': 'Mexicana'
+      },
+      {
+        'nombre': 'Maria',
+        'edad': 17,
+        'nacionalidad': 'Española'
+      },
+      {
+        'nombre': 'Tom',
+        'edad': 18,
+        'nacionalidad': 'Estadounidense'
+      },
+      {
+        'nombre': 'Ana',
+        'edad': 45,
+        'nacionalidad': 'Colombiana'
+      },
+      {
+        'nombre': 'Mohammed',
+        'edad': 16,
+        'nacionalidad': 'Marroquí'
+      },
+      {
+        'nombre': 'Elena',
+        'edad': 28,
+        'nacionalidad': 'Francesa'
+      },
+      {
+        'nombre': 'Raul',
+        'edad': 9,
+        'nacionalidad': 'Argentino'
+      },
+      {
+        'nombre': 'Jasmine',
+        'edad': 29,
+        'nacionalidad': 'Canadiense'
+      },
+      {
+        'nombre': 'Sara',
+        'edad': 16,
+        'nacionalidad': 'Italiana'
+      },
+      {
+        'nombre': 'Alejandro',
+        'edad': 35,
+        'nacionalidad': 'Chileno'
+      }
+  ]
+
+  context = {
+    'title': 'Owner List',
+    'owners': data_context,
+  }
+  return render(request, './owner/owner_list.html', context)
